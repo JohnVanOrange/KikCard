1d6ef7c398665c3be39489e72d2984e8485309c3
Cameron Bulock <cameron@bulock.com>
2014-01-31 13:30:30 -0500

    move all js to after dom is loaded

--- res/js/main.js
+++ res/js/main.js
@@ -1,59 +1,58 @@
-var api = {
-	client : function (method, callback, opt) {
-		if (!opt) {
-			opt = {test: 'test'};
-		}
-		var url = 'http://prettylovable.com/api/' + method;
-		$.ajax({
-			type: 'post',
-			async: false,
-			url: url,
-			data: opt,
-			dataType: 'json',
-			success: function(response) {
-				try {
-					if (response.hasOwnProperty('error')) {
-						throw {name: response.error, message: response.message};
-					}
-					callback(response);
-					if (response.message) {
-					}
-				} catch(e) {
-				}
-			}
-		});
-	},
-	call : function (method, callback, opt) {
-		return this.client(method, callback, opt);
-	}
-};
-
-var image = {
-		next : function() {
-			$('#main').attr('src', $('#main').attr('data-next-src'));
-			$('#main').attr('data-uid', $('#main').attr('data-next-uid'));
-			this.random();
-		},
-    random : function() {
-      api.call('image/random', function(data){
-				$('#main').attr('data-next-uid', data.uid);
-				$('#main').attr('data-next-src', data.image_url);
-				var next_image = new Image();
-				next_image.src = data.image_url;
-				
-      });
-      return;
-    },
-		load : function(uid) {
-      api.call('image/get', function(data){
-				$('#main').attr('src', data.image_url);
-				$('#main').attr('data-uid', data.src);
-      }, {image: uid});    
-		}
-}
-
-
 $(document).ready(function() {
+	var api = {
+		client : function (method, callback, opt) {
+			if (!opt) {
+				opt = {test: 'test'};
+			}
+			var url = 'http://prettylovable.com/api/' + method;
+			$.ajax({
+				type: 'post',
+				async: false,
+				url: url,
+				data: opt,
+				dataType: 'json',
+				success: function(response) {
+					try {
+						if (response.hasOwnProperty('error')) {
+							throw {name: response.error, message: response.message};
+						}
+						callback(response);
+						if (response.message) {
+						}
+					} catch(e) {
+					}
+				}
+			});
+		},
+		call : function (method, callback, opt) {
+			return this.client(method, callback, opt);
+		}
+	};
+	
+	var image = {
+			next : function() {
+				$('#main').attr('src', $('#main').attr('data-next-src'));
+				$('#main').attr('data-uid', $('#main').attr('data-next-uid'));
+				this.random();
+			},
+			random : function() {
+				api.call('image/random', function(data){
+					$('#main').attr('data-next-uid', data.uid);
+					$('#main').attr('data-next-src', data.image_url);
+					var next_image = new Image();
+					next_image.src = data.image_url;
+					
+				});
+				return;
+			},
+			load : function(uid) {
+				api.call('image/get', function(data){
+					$('#main').attr('src', data.image_url);
+					$('#main').attr('data-uid', data.src);
+				}, {image: uid});    
+			}
+	}
+		
 	image.random();
 	
 	if (cards.kik.message) {
